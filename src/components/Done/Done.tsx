c32ffc2d78542cdf17a9dce1c8471921d59154b1
Kirill Mecker <ghost3299@yandex.ru>
2021-02-06 17:42:30 +0400

    feat: cost calc

--- src/components/Done/Done.tsx
+++ src/components/Done/Done.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import  { Badge, Card } from 'react-bootstrap';
+import { costCalculation } from '../../utils/costCalculation';
+import {doneDeeds} from'../../lists-deeds/doneDeeds'
 import './Done.css'
 
 function Done (){
     return(<div className='done'>
     <div className='done__header'>
-    <Badge style={{borderRadius: '50%', display: 'flex'}} variant="secondary" className='badger'></Badge>
+    <Badge style={{borderRadius: '50%', display: 'flex'}} variant="secondary" className='badger'>{doneDeeds.length}</Badge>
         <h2 className='done__title'>Done</h2>
     </div> 
     <div>
-    <Card className='done__card'>
-        <Card.Body style={{padding: '0.5rem 1.25rem 0.5rem 0.5rem'}} className='card__body'>
-        <div style={{height: '100%'}}>
-           <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-            </Card.Text>
-            </div>
-        </Card.Body>
-    </Card>
+        {doneDeeds.map((item)=>{ return(
+            <Card className='done__card'>
+            <Card.Body style={{padding: '0.5rem 1.25rem 0.5rem 0.5rem'}} className='card__body'>
+            <div style={{height: '100%'}}>
+               <Card.Title>{item.title}</Card.Title>
+                <Card.Text>{
+                costCalculation(item.end-item.start)
+                    }
+                </Card.Text>
+                </div>
+            </Card.Body>
+        </Card>)
+        })}
+    
     </div>
     </div>);
 }
--- src/lists-deeds/doneDeeds.js
+++ src/lists-deeds/doneDeeds.js
@@ -1,1 +1,4 @@
-export const doneDeeds = [{ title: 'Go to grocery' }];
+export const doneDeeds = [
+  { title: 'Take out the trash', start: 1612611882710, end: 1612613739036 },
+  { title: 'Walk the dog', start: 1612613593140, end: 1612613739036 },
+];
